f7e306a5cb9ed645382d77b7cedc743a3176c343
MinTo96 <47050227+MinTo96@users.noreply.github.com>
2021-03-02 14:48:44 +0900

    add background click event

--- menu.js
+++ menu.js
@@ -1,23 +1,34 @@
 const menuBtn = document.querySelector("#menu-btn"),
     sideMenu = document.querySelector("#side-menu"),
     sideBackground = document.querySelector("#side-background"),
-    sideMenuQuitBtn = document.querySelector("#side-menu-quit-btn");
+    sideMenuQuitBtn = document.querySelector("#side-menu-quit-btn"),
+    footerBar = document.querySelector("#time-js");
 
-const SCREEN = "show";
+const SCREEN_SHOW = "show";
+const SCREEN_HIDE = "invisible";
 
 function clickMenuBtn() {
-    sideMenu.classList.toggle(SCREEN);
-    sideBackground.classList.toggle(SCREEN);
+    sideMenu.classList.toggle(SCREEN_SHOW);
+    sideBackground.classList.toggle(SCREEN_SHOW);
+    footerBar.classList.toggle(SCREEN_HIDE);
 }
 
 function clickMenuQuitBtn() {
-    sideMenu.classList.toggle(SCREEN);
-    sideBackground.classList.toggle(SCREEN);
+    sideMenu.classList.toggle(SCREEN_SHOW);
+    sideBackground.classList.toggle(SCREEN_SHOW);
+    footerBar.classList.toggle(SCREEN_HIDE);
+}
+
+function clickBackground() {
+    sideMenu.classList.toggle(SCREEN_SHOW);
+    sideBackground.classList.toggle(SCREEN_SHOW);
+    footerBar.classList.toggle(SCREEN_HIDE);
 }
 
 function init() {
     menuBtn.addEventListener("click", clickMenuBtn);
     sideMenuQuitBtn.addEventListener("click", clickMenuQuitBtn);
+    sideBackground.addEventListener("click", clickBackground);
 }
 
 init();
