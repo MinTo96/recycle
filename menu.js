6acf0a8c342f353387e62f2f56f378d36007bf34
MinTo96 <47050227+MinTo96@users.noreply.github.com>
2021-03-14 09:04:29 +0900

    JS: Scroll fixed

--- menu.js
+++ menu.js
@@ -1,28 +1,37 @@
+'use strict'
+
 const menuBtn = document.querySelector("#menu-btn"),
     sideMenu = document.querySelector("#side-menu"),
     sideBackground = document.querySelector("#side-background"),
     sideMenuQuitBtn = document.querySelector("#side-menu-quit-btn"),
-    footerBar = document.querySelector("#time-js");
+    footerBar = document.querySelector("#time-js"),
+    body = document.querySelector("body");
 
 const SCREEN_SHOW = "show";
 const SCREEN_HIDE = "invisible";
+const SCREEN_FIXED = "fixed";
 
 function clickMenuBtn() {
     sideMenu.classList.toggle(SCREEN_SHOW);
     sideBackground.classList.toggle(SCREEN_SHOW);
     footerBar.classList.toggle(SCREEN_HIDE);
+    body.classList.toggle(SCREEN_FIXED);
 }
 
 function clickMenuQuitBtn() {
     sideMenu.classList.toggle(SCREEN_SHOW);
     sideBackground.classList.toggle(SCREEN_SHOW);
     footerBar.classList.toggle(SCREEN_HIDE);
+    body.classList.toggle(SCREEN_FIXED);
+
 }
 
 function clickBackground() {
     sideMenu.classList.toggle(SCREEN_SHOW);
     sideBackground.classList.toggle(SCREEN_SHOW);
     footerBar.classList.toggle(SCREEN_HIDE);
+    body.classList.toggle(SCREEN_FIXED);
+
 }
 
 function init() {
